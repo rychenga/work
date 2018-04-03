4fe0bd985c4ff16a5ef9364eea3f0bb264f88e95
rychenga <s813031ranyi@yahoo.com.tw>
2018-04-03 10:57:12 +0800

    Create work.cs

---
+++ work.cs
@@ -0,0 +1,117 @@
+----------------------------------------------
+--2018/03/26
+--[oracle]
+http://127.0.0.1:8080/apex/f?p=4950:1:2429849603166661
+--------------------------------------------------------------------------
+CREATE TABLESPACE TBS
+datafile 'C:\oraclexe\app\oracle\oradata\XE\TBS.dbf' SIZE 10M;
+--------------------------------------------------------------------------
+create TABLESPACE TBS
+datafile 'C:\oraclexe\app\oracle\oradata\XE\TBS.dbf' SIZE 10M
+ AUTOEXTEND ON
+    NEXT 10M
+    MAXSIZE 1000M;
+--------------------------------------------------------------------------
+create TABLESPACE TBS
+datafile 'C:\oraclexe\app\oracle\oradata\XE\TBS.dbf' SIZE 10M
+ AUTOEXTEND ON
+    NEXT 1000M
+    MAXSIZE 10000M;
+--------------------------------------------------------------------------
+
+SELECT A.STOCK_NAME,A.NAME,COUNT(*) CN
+FROM LEGAL A,I_TRUST B
+WHERE A.STOCK_NAME = B.STOCK_NAME
+AND A.PRICE=B.PRICE
+AND A.TYPE=B.TYPE
+AND A.DAY = B.DAY
+AND A.TYPE = 'BUY'
+AND A.SHEETS > 5000
+AND A.CHANGE > 0
+AND A.DAY >= SYSDATE -14
+GROUP BY A.STOCK_NAME,A.NAME
+ORDER BY CN
+
+SELECT *
+FROM LEGAL A,I_TRUST B
+WHERE  A.STOCK_NAME = B.STOCK_NAME
+AND A.NAME = B.NAME
+AND A.PRICE=B.PRICE
+AND A.TYPE=B.TYPE
+AND A.DAY = B.DAY
+AND A.TYPE = 'BUY'
+AND A.STOCK_NAME = '2317'
+AND A.DAY >= SYSDATE -14
+ORDER BY A.DAY
+
+
+select t1.*,t2.sheets,t2.type,t3.sheets,t3.type 
+from LEGAL t1,I_TRUST t2,DEALERS t3
+where t1.stock_name = t2.stock_name
+and t2.stock_name = t3.stock_name
+and t1.price =t2.price
+and t1.price = t3.price
+and t1.day =t2.day
+and t1.day = t3.day
+and t1.change > 0
+AND t1.DAY >= SYSDATE -7
+order by t1.stock_name,t1.day
+
+
+----------------------------------------
+select T1.NAME
+from I_TRUST t1,LEGAL T2
+WHERE T1.DAY = T2.DAY
+AND T1.TYPE = T2.TYPE
+AND T1.NAME = T2.NAME
+AND T1.DAY = TO_DATE('2014/08/12','YYYY/MM/DD')
+AND T1.TYPE = 'BUY'
+;
+
+select T1.NAME
+from I_TRUST t1,LEGAL T2
+WHERE T1.DAY = T2.DAY
+AND T1.NAME = T2.NAME
+AND T1.DAY = TO_DATE('2014/08/12','YYYY/MM/DD')
+AND T1.TYPE <>  T2.TYPE
+;
+
+select T1.NAME,COUNT(*)CN
+from LEGAL t1
+WHERE T1.DAY >= SYSDATE -30
+AND T1.NAME IN (
+
+)
+GROUP BY T1.NAME
+;
+
+select T1.NAME,COUNT(*)CN
+from I_TRUST t1
+WHERE T1.DAY >= SYSDATE -30
+AND T1.NAME IN (
+
+)
+GROUP BY T1.NAME
+;
+--------------------------------------------------------------------------
+--2018/03/30
+--〔c#〕
+//list 一維陣列
+public List<string> columns = new List<string>();
+private List<string> _tempRows = new List<string>();
+
+//list 二維陣列
+public List<List<string>> rows = new List<List <string>>();
+
+//get rowdata by 2 Array
+foreach (System.Data.DataRow pri in GetDataTable.Rows)
+{    
+    _tempRows = new List<string>();
+
+    foreach (System.Data.DataColumn col in GetDataTable.Columns)
+    {
+        //Console.WriteLine("{0}:{1}", col, pri[col]);
+        _tempRows.Add(pri[col].ToString());
+    }
+    rows.Add(_tempRows);
+}
