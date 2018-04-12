3d0932f3a80d7c069e74680ed845c39106adaa61
rychenga <s813031ranyi@yahoo.com.tw>
2018-04-12 10:55:13 +0800

    Update work.cs

--- work.cs
+++ work.cs
@@ -197,3 +197,37 @@ namespace LineQ
         }
     }
 }
+
+//Linq group by
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LineQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Specify the data source. 
+            int[] scores = new int[] { 97, 92, 81, 60 ,20,10,100,101,155,62,88,90,90,100,100};
+
+            // Define the query expression.
+            var scoreQuery =
+                from score in scores
+                where score > 80
+                orderby score descending
+                group score by score into grp
+                select new { name = grp.Key, cnt = grp.Count() };
+
+            // Execute the query. 
+            foreach (var i in scoreQuery)
+            {
+                Console.WriteLine(i.name + " "+i.cnt);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
